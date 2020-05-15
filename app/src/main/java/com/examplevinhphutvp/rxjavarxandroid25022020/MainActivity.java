d4b68061ed57774c9a7560676053e486c688d45b
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-05-16 00:15:54 +0700

    finish

--- app/src/main/java/com/examplevinhphutvp/rxjavarxandroid25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/rxjavarxandroid25022020/MainActivity.java
@@ -3,6 +3,17 @@ package com.examplevinhphutvp.rxjavarxandroid25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Bước 1 : tạo ra 1 observable
+        Observable<String> observableString =
+                Observable.just("1","2","3","4","5","6");
+        // Buoc 2 : Lang nghe du lieu
+        observableString
+                .subscribeOn(Schedulers.io())
+                .map(new Function<String, Integer>() {
+            @Override
+            public Integer apply(String s) throws Throwable {
+//                Toast.makeText(MainActivity.this, s + "", Toast.LENGTH_SHORT).show();
+                return Integer.parseInt(s);
+            }
+        })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Integer integer) {
+                        Toast.makeText(MainActivity.this,  integer + "",Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
+
 }
